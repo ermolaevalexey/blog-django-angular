99b7c7b71abc412bc408078c1742a89577e90d53
Алексей Ермолаев <aleksejermolaev@MacBook-Pro-Aleksej.local>
2016-06-23 13:56:48 +0300

    ~ added author field to api

--- blog2/api.py
+++ blog2/api.py
@@ -1,20 +1,24 @@
 # blog2/api.py
 from django.contrib.auth.models import User
 from tastypie import fields
+from django.db import models
 from tastypie.authorization import DjangoAuthorization, Authorization
 from tastypie.authentication import BasicAuthentication
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from blog2.models import Post
 
 class UserResource(ModelResource):
+
+    posts = fields.ToManyField('blog2.api.resources.PostResource', 'posts')
+
     class Meta:
         queryset = User.objects.all()
         resource_name = 'auth/user'
         fields = ['username', 'first_name', 'last_name', 'email', 'last_login']
         excludes = ['password', 'is_active', 'is_staff', 'is_superuser']
         allowed_methods = ['get', 'post']
-        authorization = DjangoAuthorization()
-        authentication = BasicAuthentication()
+        #authorization = Authorization()
+        #authentication = BasicAuthentication()
         filtering = {
             'username': ALL
         }
@@ -29,10 +33,14 @@ class PostResource(ModelResource):
     # 'user' on the next line of code is the Tastypie field name, the 2nd
     # appearance tells the `ForeignKey` it maps to the `user` attribute of
     # `Entry`. Field names and model attributes don't have to be the same.
+
+    author = fields.ToOneField(UserResource, 'author')
+
     class Meta:
         queryset = Post.objects.all()
         resource_name = 'post'
         authorization = Authorization()
+        authentication = BasicAuthentication()
         allowed_methods = ['get', 'post']
         filtering = {
             'user': ALL_WITH_RELATIONS,
@@ -45,7 +53,7 @@ class CurrentUserResource(ModelResource):
         resource_name = 'current_user'
         fields = ['username', 'first_name', 'last_name', 'email', 'last_login']
         allowed_methods = ['get', 'post']
-        authorization = DjangoAuthorization()
+        authorization = Authorization()
         authentication = BasicAuthentication()
 
     def get_object_list(self, request):

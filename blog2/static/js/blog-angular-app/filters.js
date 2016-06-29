41b3ab928ca1c884b69525e582f91081ad126f96
Алексей Ермолаев <aleksejermolaev@MacBook-Pro-Aleksej.local>
2016-06-29 13:09:36 +0300

    ~ set anonymous function to filters

--- blog2/static/js/blog-angular-app/filters.js
+++ blog2/static/js/blog-angular-app/filters.js
@@ -3,14 +3,16 @@
  */
 'use strict';
 
-var blogFilters = angular.module('blogFilters', []);
+(function (exports) {
+    exports.blogFilters = angular.module('blogFilters', [])
 
-blogFilters.filter('limitText', function () {
-   return function (str, len) {
-       if(str.length > len) {
-           str = str.substr(0, len);
-           str += '...';
+    .filter('limitText', function () {
+       return function (str, len) {
+           if(str.length > len) {
+               str = str.substr(0, len);
+               str += '...';
+           }
+           return str;
        }
-       return str;
-   }
-});
+    });
+})(this);
